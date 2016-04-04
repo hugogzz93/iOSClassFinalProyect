e7eb5e3a23daf394f24b339c3e739f567b019a70
Hugo Gonzalez <pinelo93@gmail.com>
2016-04-04 10:35:08 -0500

    Adds basic interface.

--- iOSProyectoFinal/ViewController.swift
+++ iOSProyectoFinal/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var picker1 = ["++", "--"]
+    var picker2 = ["+=", "-="]
 
+    @IBOutlet weak var pickerView1: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +23,18 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return picker1.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return picker1[row]
     }
 
 
