cf5a5902aa8b1b541b511ed1fa6a719317fcc395
Hugo Gonzalez <pinelo93@gmail.com>
2016-04-28 20:46:03 -0500

    Major refactoring, still unfinished

--- iOSProyectoFinal/View.swift
+++ iOSProyectoFinal/View.swift
@@ -8,6 +8,206 @@
 
 import UIKit
 
-class View: UIView {
-
+protocol ViewHandler {
+    func setCurrentIns(num: Int)
+    func getCurrentInstruction() -> Int
+    func updateCout(output : String, position: Int)
+    func updateQuiz()
 }
+
+protocol ControllerDelegate {
+    func executeInstruction()
+    func getExecutionFinished() -> Bool
+    func getData() -> Data
+    func restart()
+}
+
+
+class View: UIView, ControllerDelegate {
+
+	var forHeader = ForHeader()
+    
+    let arraySize = 10
+    var forConditional = true
+    var ifConditional = true
+    var executionFinished = false
+    var forInitialized = false
+    
+    var data = Data()
+    var delegate : ViewHandler!
+
+
+    //MARK: Operation Handlers
+
+    func binaryOperation(operatorSign: String, leftOperand: Float, rightOperand: Float) -> Bool {
+    	var result : Bool
+    	switch operatorSign {
+	        case "<":
+	            result = leftOperand < rightOperand
+	        case "<=":
+	            result = leftOperand <= rightOperand
+	        case ">":
+	            result = leftOperand > rightOperand
+	        case ">=":
+	            result = leftOperand >= rightOperand
+	        case "==":
+	            result = leftOperand == rightOperand
+	        case "!=":
+	            result = leftOperand != rightOperand
+	        default: break
+	    }
+
+    	return result
+    }
+
+    func mutation(sign: String, numberName: Array<String>) -> Float {
+    	var number = getData(numberName[0])
+        switch sign {
+            case "++":
+                number += 1
+            case "--":
+                number -= 1
+            default: break
+        }
+        return number
+    }
+
+    func expression(sign: String, operands: Array<String>) -> Float {
+    	var leftOperand = getData(operands[0])
+    	let rightOperand : Float = NSString(string: operands[1]).floatValue
+
+    	switch sign {
+	        case "+=":
+	            leftOperand += rightOperand
+	        case "-=":
+	            leftOperand -= rightOperand
+	        default: break
+        }
+        return leftOperand
+    }
+
+    func getData(name: String) -> Float {
+    	var number : Float
+    	switch name {
+	        case "v1":
+	            number = data.v1
+	        case "v2":
+	            number = data.v2
+	        case "v3":
+	            number = data.v3
+	        default: number = -1
+        }
+        return number
+    }
+
+    func updateData(name: String, number: Float) {
+    	switch name {
+	        case "v1":
+	        	data.v1 = number
+	        case "v2":
+	        	data.v2 = number
+	        case "v3":
+	        	data.v3 = number
+	        default: break
+        }
+    }
+
+    func updateVariable(names: Array<String>, sign: String, instruction: (String, Array<String>) -> Float) {
+        updateData(names[0], number: instruction(sign, names))
+    }
+
+    //MARK: Concrete Methods
+    
+    func finalizeExecution() {
+        executionFinished = true
+        setCurrentArrow(-1)
+    }
+
+    func handleCoutIns() {
+        coutHandler("v1", position: 0)
+        coutHandler("v2", position: 1)
+        coutHandler("v3", position: 2)
+        setCurrentIns(0)
+    }
+
+    func coutHandler(name: String, position: Int) {
+        // var outPut = viewList[currentView].getData(name)
+        var outPut = getData(name);
+        if outPut == -1 {
+            outPut = 0.00
+        }
+
+        switch position {
+            case 0: delegate.updateCout(String(outPut), position: 0)
+            case 1: delegate.updateCout(String(outPut), position: 1)
+            case 2: delegate.updateCout(String(outPut), position: 2)
+            default:break;
+        }
+    }
+    
+    func setCurrentArrow(num : Int) {
+        
+        
+        for pointer in getPointerCollection() {
+            pointer.hidden = true
+        }
+        
+        if num != -1 {
+            getPointerCollection()[num].hidden = false
+        }
+        
+    }
+
+    //MARK: Protocol ControllerDelegate
+    
+    func restart() {
+        data.v1 = 0
+        data.v2 = 0
+        data.v3 = 0
+        updateOutlets()
+        executionFinished = false
+        forInitialized = false
+        setCurrentIns(0)
+    }
+
+    func setCurrentIns(num: Int) {
+        if !executionFinished {
+            delegate.setCurrentIns(num)
+            
+            setCurrentArrow(num)
+        }
+    }
+
+    func getExecutionFinished() -> Bool {
+        return executionFinished
+    }
+
+    func getData() -> Data {
+        return data
+    }
+
+    //MARK: Abstract Methods
+
+    func updateOutlets() {
+    	preconditionFailure("This method must be overridden") 
+    }
+
+    func executeInstruction() {
+    	preconditionFailure("This method must be overridden") 
+    }
+
+    func handleInstruction() {
+    	preconditionFailure("This method must be overridden") 
+    }
+
+    func currentInstruction() -> Int {
+        return delegate.getCurrentInstruction()
+    }
+    
+    func getPointerCollection() -> Array<UIView> {
+        preconditionFailure("This method must be overridden")
+    }
+}
+
+    
+
--- iOSProyectoFinal/ViewFull.swift
+++ iOSProyectoFinal/ViewFull.swift
@@ -10,4 +10,216 @@ import UIKit
 
 class ViewFull: View {
 
+	let AFTER_IF = 5
+	let IF_START = 2
+    
+    @IBOutlet var pointerCollection: Array<UIView>?
+    
+    @IBOutlet weak var mainV3: UITextField!    
+    
+    //for init
+    @IBOutlet weak var forInitV1P: UITextField!
+    @IBOutlet weak var forInitV2P: UITextField!
+    
+    @IBOutlet weak var forInitV1Condition: UITextField!
+    @IBOutlet weak var forInitV1NumberField: UITextField!
+    
+    @IBOutlet weak var forInitV1IncDec: UITextField!
+    @IBOutlet weak var forInitV2IncDec: UITextField!
+    
+    //for body
+    @IBOutlet weak var forV1IncDec: UITextField!
+    @IBOutlet weak var forV2IncDec: UITextField!
+    @IBOutlet weak var forV3Mutation: UITextField!
+    @IBOutlet weak var forV3NumberField: UITextField!
+    
+    @IBOutlet weak var coutField1: UITextField!
+    @IBOutlet weak var coutField2: UITextField!
+    @IBOutlet weak var coutField3: UITextField!
+    
+    //if conditional
+    @IBOutlet weak var ifConditionP1: UITextField!
+    @IBOutlet weak var ifConditionP2: UITextField!
+    
+    //if body
+    @IBOutlet weak var ifV1IncDec: UITextField!
+    @IBOutlet weak var ifV2IncDec: UITextField!
+    @IBOutlet weak var ifV3Mutation: UITextField!
+    @IBOutlet weak var ifV3NumberField: UITextField!
+    
+    //quiz fields
+    @IBOutlet var quizLb: UILabel!
+    @IBOutlet var quizv1: UITextField!
+    @IBOutlet var quizv2: UITextField!
+    @IBOutlet var quizv3: UITextField!
+    @IBOutlet var quizBck: UIView!
+
+	// MARK: Protocol
+
+	override func updateOutlets() {
+        mainV3.text = String(forHeader.mainV3)
+        
+        forInitV1P.text = String(forHeader.forInitV1P)
+        forInitV2P.text = String(forHeader.forInitV2P)
+        
+        forInitV1Condition.text = forHeader.forInitV1Condition
+        forInitV1NumberField.text = String(forHeader.forInitV1NumberField)
+        
+        forInitV1IncDec.text = forHeader.forInitV1IncDec
+        forInitV2IncDec.text = forHeader.forInitV2IncDec
+        
+        //for body
+        forV1IncDec.text = forHeader.forV1IncDec
+        forV2IncDec.text = forHeader.forV2IncDec
+        forV3Mutation.text = forHeader.forV3Mutation
+        forV3NumberField.text = String(forHeader.forV3NumberField)
+        
+        coutField1.text = forHeader.coutField1
+        coutField2.text = forHeader.coutField2
+        coutField3.text = forHeader.coutField3
+        
+        //if conditional
+        ifConditionP1.text = forHeader.ifConditionP1
+        ifConditionP2.text = forHeader.ifConditionP2
+        
+        //if body
+        ifV1IncDec.text = forHeader.ifV1IncDec
+        ifV2IncDec.text = forHeader.ifV2IncDec
+        ifV3Mutation.text = forHeader.ifV3Mutation
+        ifV3NumberField.text = forHeader.ifV3NumberField
+    }
+
+    override func executeInstruction() {
+        if !executionFinished {
+                handleInstruction()
+                setCurrentIns(currentInstruction() + 1)
+        }   
+    }
+
+    func handleInstruction(number: Int) {
+        switch number {
+        case 0:
+            data.v3 = Float(forHeader.mainV3)
+        case 1:
+            if !forInitialized {
+                data.v1 = Float(forHeader.forInitV1P)
+                data.v2 = Float(forHeader.forInitV2P)
+                forInitialized = true
+            } else {
+                handleForConditional()
+            }
+            
+        case 2:
+            handleConditional()
+        case 3:
+            handleIfIns(1)
+        case 4:
+            handleIfIns(2)
+        case 5:
+            handleIfIns(3)
+        case 6:
+            handleLoopIns(1)
+        case 7:
+            handleLoopIns(2)
+        case 8:
+            handleLoopIns(3)
+        case 9:
+            handleCoutIns()
+            
+        default: break
+            
+        }
+    }
+
+    //MARK: Handlers
+
+    func handleForConditional() {
+        
+        forConditional = binaryOperation(forHeader.forInitV1Condition, leftOperand: data.v1, rightOperand: Float(forHeader.forInitV1NumberField))
+        
+        if forConditional {
+            var name = [String(data.v1)], name2 = [String(data.v2)]
+        	data.v1 = mutation(forHeader.forInitV1IncDec, numberName: name)
+        	data.v2 = mutation(forHeader.forInitV2IncDec, numberName: name2)
+        } else {
+            finalizeExecution()
+        }
+    }
+
+    func handleConditional() {
+        var number : Float = 0
+        number = getData(forHeader.ifConditionP2)
+        ifConditional = binaryOperation(forHeader.ifConditionP1, leftOperand: data.v1, rightOperand: number)
+        
+        if !ifConditional {
+            setCurrentIns(AFTER_IF)
+        } else {
+            setCurrentIns(IF_START)
+        }
+    }
+
+    func handleIfIns(Ins: Int) {
+    	var varNames = Array<String>(), sign = ""
+    	var instruction = (String, Array<String>) -> Float
+    	switch Ins {
+	    	case 1: 
+	    		varNames = ["v1"]
+	    		sign = forHeader.ifV1IncDec
+	    		instruction = mutation
+	    	case 2: 
+	    		varNames = ["v2"]
+	    		sign = forHeader.ifV2IncDec
+	    		instruction = mutation
+	    	case 3: 
+	    		varNames = ["v3", forHeader.ifV3NumberField]
+	    		sign = forHeader.ifV3Mutation
+	    		instruction = expression
+	    	default:break
+    	}
+
+    	updateVariable(varNames, sign, instruction)
+    }
+
+    func handleLoopIns(Ins: Int) {
+    	var varNames = Array<String>(), sign = ""
+    	var instruction = () -> ()
+    	switch Ins {
+	    	case 1: 
+	    		varNames = ["v1"]
+	    		sign = forHeader.forV1IncDec
+	    		instruction = mutation()
+	    	case 2: 
+	    		varNames = ["v2"]
+	    		sign = forHeader.forV2IncDec
+	    		instruction = mutation()
+	    	case 3: 
+	    		varNames = ["v3", forHeader.forV3NumberField]
+	    		sign = forHeader.forV3Mutation
+	    		instruction = expression()
+	    	default:break
+    	}
+
+    	updateVariable(varNames, sign, instruction)
+    }
+
+    // MARK: Flow Control
+    func setCurrentIns(num: Int) {
+        if !executionFinished {
+            delegate.setCurrentIns(num)
+            
+            for pointer in pointerCollection! {
+                pointer.hidden = true
+            }
+            pointerCollection![num].hidden = false
+        }
+    }
+
+// forHeader.ifV1IncDec
+
+    // func updateVariable(name: String, sign: String, instruction: (String, Int) -> Int) {
+    //     updateData(name, instruction(sign, getData(name)))
+    // }
+
+
+
 }
